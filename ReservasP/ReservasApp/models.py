458a64c27f4dd18e52717aa4066ccc9a3a637ec7
jaredolmosmb <jared@mb.company>
2023-05-12 11:15:36 -0600

    add qr librarys and extra code to add qr to model Reservacion

--- ReservasP/ReservasApp/models.py
+++ ReservasP/ReservasApp/models.py
@@ -6,6 +6,8 @@ from django.conf import settings
 from django.utils import timezone
 import uuid, random
 from django.contrib.auth.models import Group
+import qrcode
+from io import BytesIO
 
 from django.utils.translation import gettext_lazy as _
 
@@ -26,6 +28,20 @@ class Reservacion(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+    def save(self, *args, **kwargs):
+        # generar el código QR
+        qr = qrcode.QRCode(version=1, box_size=10, border=4)
+        qr.add_data(str(self.id))
+        qr.make(fit=True)
+        img = qr.make_image(fill_color='black', back_color='white')
+
+        # guardar el código QR como imagen en un campo de la instancia del modelo
+        buffer = BytesIO()
+        img.save(buffer)
+        self.qr_code.save(f'{self.id}.png', File(buffer), save=False)
+
+        super().save(*args, **kwargs)
 
 class ReservacionHistorico(models.Model):
     hora_inicio = models.DateTimeField()
